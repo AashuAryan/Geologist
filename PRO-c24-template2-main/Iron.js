2936d73e50611a4565aaea8373467abf678cc999
AashuAryan <82717211+AashuAryan@users.noreply.github.com>
2022-08-22 19:17:15 +0530

    Add files via upload

---
+++ PRO-c24-template2-main/Iron.js
@@ -0,0 +1,11 @@
+class Iron{
+	constructor(){
+		var options={
+			restitution:0.8,
+			friction:3,
+		    density:30
+		}
+
+
+    }
+}
---
+++ PRO-c24-template2-main/Stone.js
@@ -0,0 +1,11 @@
+class Stone{
+	constructor(){
+		var options={
+			restitution:0.8,
+			friction:0.9,
+		    density:12
+		}
+
+
+    }
+}
